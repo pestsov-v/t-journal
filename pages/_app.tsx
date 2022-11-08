f924cd3dacabe8d4eabaf612bbc05aee020901b3
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-09 00:51:40 +0300

    react fragment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import React from "react";
 import '../styles/globals.css';
 import {AppProps} from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps}: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Next create</title>
+        <link key={1} rel="icon" href="/favicon2.ico"/>
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
