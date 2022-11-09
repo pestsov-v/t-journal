78a087d653f8e3a414da17be958b4e7d91a40ed5
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-10 00:27:48 +0300

    styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,8 @@ function MyApp({ Component, pageProps}: AppProps): JSX.Element {
       <Head>
         <title>Next practice</title>
         <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&family=Nunito:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
