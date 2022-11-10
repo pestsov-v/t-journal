5b2e63191653294462e06685c79ee60fc3149afd
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-10 23:52:54 +0300

    buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {HTag} from "../components";
+import {Button, HTag} from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <HTag tag='h1'>Text</HTag>
+        <Button appearance='primary'>Кнопка</Button>
+        <Button appearance='ghost'>Кнопка</Button>
     </>
   );
 }
