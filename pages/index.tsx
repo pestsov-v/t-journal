535ce2d60e3fd70d741256675ae55406d4e2521e
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-13 01:48:00 +0300

    ref arch

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import {Button, HTag, Paragraph, Tag} from "../components";
 
 export default function Home(): JSX.Element {
+    const [counter, setCounter] = React.useState<number>(0)
+
+
   return (
     <>
-      <HTag tag='h1'>Text</HTag>
-        <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <HTag tag='h1'>{counter}</HTag>
+        <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
         <Button appearance='ghost' arrow='right'>Кнопка</Button>
       <Paragraph size='big'>Big</Paragraph>
       <Paragraph>Middle</Paragraph>
