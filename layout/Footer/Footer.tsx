5c6af904ea71518c6d4e39b653ffbc813d748f4e
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-13 21:16:52 +0300

    footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import {FooterProps} from "./Footer.props";
+import cn from 'classnames';
+import styles from './Footer.module.css';
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                Top 2022 - {new Date().getFullYear()} Все права защищены
+            </div>
+            <a href='#' target='_blank'>Пользовательское соглашение</a>
+            <a href='#' target='_blank'>Политика конфиденциальности</a>
+        </footer>
     );
 };
