58dad4004b8f83ddb95acf3b40138a5b84f27156
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-16 00:04:54 +0300

    getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
 import {Button, HTag, Paragraph, Rating, Tag} from "../components";
 import {WithLayout} from "../layout/Layout";
+import {GetStaticProps} from "next";
+import axios from "axios";
+import {MenuItem} from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({menu, firstCategory}: HomeProps): JSX.Element {
     const [counter, setCounter] = React.useState<number>(0);
 
     const [rating, setRating] = useState<number>(4);
@@ -21,8 +24,32 @@ function Home(): JSX.Element {
         <Tag size='small' color='green'>Зелёный</Tag>
         <Tag size='middle' color='primary'>Приоритетный</Tag>
         <Rating rating={rating} isEditable={true} setRating={setRating}/>
+        <ul>
+            {menu.map(m => <li key={m._id.secondCategory}>{m._id.secondCategory}</li>)}
+        </ul>
     </>
   );
 }
 
 export default WithLayout(Home);
+
+// TODO разобраться с типизацией картинок и приватными переменными
+
+export const getStaticProps: GetStaticProps = async () => {
+    const firstCategory = 0;
+    const {data: menu} = await axios.post<MenuItem[]>('https://courses-top.ru' + '/api/top-page/find', {
+        firstCategory
+    });
+
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    };
+};
+
+interface HomeProps extends Record<string, unknown>{
+    menu: MenuItem[]
+    firstCategory: number
+}
